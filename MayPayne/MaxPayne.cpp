9d7d561c19e713107c43d7d4fbf834b5f479c76c
visuve <veli-matti.visuri@iki.fi>
2022-02-19 16:07:31 +0200

    Better hacks for Max Payne

--- MayPayne/MaxPayne.cpp
+++ MayPayne/MaxPayne.cpp
@@ -6,12 +6,31 @@
 	e0b3b859c28adbf510dfc6285e1667173aaa7b05ac66a62403eb96d50eefae7b
 */
 
-int wmain()
+int wmain(int argc, wchar_t** argv)
 {
+	if (argc <= 1)
+	{
+		return ERROR_BAD_ARGUMENTS;
+	}
+
 	try
 	{
+		std::wstring argument(argv[1]);
+
 		Process process(L"maxpayne.exe");
-		process.ChangeByte<0x34829D>(X86::SubGvEv, X86::AddGvEv);
+
+		if (argument == L"reloadadds" || argument == L"infammo")
+		{
+			// Reload adds ammo instead of consumes
+			// Effectively makes unlimited painkillers & throwables
+			// process.ChangeByte<0x34829D>(X86::SubGvEv, X86::AddGvEv);
+		}
+		
+		if (argument == L"infammo")
+		{
+			// The ammo never decreases
+			process.Fill<0x357F50, 0x357F5C>(X86::Nop);
+		}
 	}
 	catch (const std::exception& e)
 	{
