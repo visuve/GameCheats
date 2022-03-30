a1fc41e9a344c8bc9f853dda9dc8ddf6a2d8c603
visuve <veli-matti.visuri@iki.fi>
2022-03-30 11:32:39 +0300

    Improve Fallout New Vegas cheats
    - The ammo pointer changes occasionally when inventory is accessed, hence using it did not work
    - Added "nowear" i.e. weapons never wear out

--- FalloutNV/FalloutNV.cpp
+++ FalloutNV/FalloutNV.cpp
@@ -21,20 +21,35 @@ int wmain(int argc, wchar_t** argv)
 
 		if (args.Contains(L"infammo"))
 		{
-			Pointer ammo = process.ResolvePointer(0x00DE0774, 0x18C, 0x120, 0x4);
-
 			ByteStream stream;
 
 			// New
-			stream << 0x3D << (ammo - 4); // cmp eax, (ammo - 4)
-			stream << 0x74 << 0x6; // jmp 6 steps forward
+			// NOTE: the ammo is probably between 0 and 255, not super reliable
+			stream << 0x8B << 0x4D << 0x08; // mov ecx, [ebp+08]
+			stream << 0x83 << 0xF9 << 0x00; // cmp ecx, 0
+			stream << 0x74 << 0x8; // je, 8 steps forward
+			stream << 0x81 << 0xF9 << 0x00 << 0x01 << 0x00 << 0x00; // cmp ecx, 255
+			stream << 0x7C << 0x3; // jl, 6 steps forward
 
 			// Stolen
-			stream << 0x8B << 0x4D << 0x08; // mov ecx, [ebp+08]
 			stream << 0x89 << 0x48 << 0x04; // mov [eax+04], ecx
 
 			process.Fill(0xC4F6A, 0xC4F6D, X86::Nop);
 			process.InjectX86(0x2ECD4A, 1, stream);
+		}
+
+		if (args.Contains(L"nowear"))
+		{
+			ByteStream stream;
+
+			// New
+			stream << 0xC7 << 0x45 << 0x08 << 0xF6 << 0x3F << 0x1C << 0x46; // mov[ebp + 08], (float)9999.99
+
+			// Stolen
+			stream << 0xD9 << 0x45 << 0x08; // fld dword ptr [ebp+08]
+			stream << 0xD9 << 0x58 << 0x0C; // fstp dword ptr [eax+0C]
+
+			process.InjectX86(0x199AE, 1, stream);
 		}
 
 		Sleep(INFINITE);
