ea0236862358dfbfb2947ffb3bd32a7d27f8c7d9
visuve <veli-matti.visuri@iki.fi>
2022-04-17 00:49:25 +0300

    Massively improve Hitman (2016) infammo cheat & add dummy ai
    - The game becomes somewhat spooky with the AI not being able to shoot :-D

--- Hitman2016/Hitman2016.cpp
+++ Hitman2016/Hitman2016.cpp
@@ -12,24 +12,38 @@ int wmain(int argc, wchar_t** argv)
 	{
 		const CmdArgs args(argc, argv,
 		{
-			{ L"infammo", L"999 ammunition always" }
+			{ L"infammo", L"Increasing ammunition" },
+			{ L"dummyai", L"AI cannot shoot" }
 		});
 
 		Process process(L"HITMAN.exe", true);
 
 		if (args.Contains(L"infammo"))
 		{
-			ByteStream code;
+			process.ChangeByte(0x103047, 0xCA, 0xC2);
+		}
 
-			// Stolen
-			code << 0x89 << 0x05 << 0x1D << 0x2C << 0xBC << 0x01;
-			code << 0x66 << 0x89 << 0x05 << 0x1A << 0x2C << 0xBC << 0x01;
-			code << 0x88 << 0x05 << 0x16 << 0x2C << 0xBC << 0x01;
-
+		if (args.Contains(L"dummyai"))
+		{
 			// New code
-			code << 0x49 << 0xBC << 0xE7 << 0x03 << 0x00 << 0x00 << 0x00 << 0x00 << 0x00 << 0x00;
+			ByteStream code;
+			code << "4C 8D A1 C8 03 00 00"; // lea r12,[rcx+000003C8]
+			code << "4D 0F B6 84 24 10 03 00 00"; // movzx r8,byte ptr [r12+00000310]
+			code << "66 41 81 F8 01 00"; // cmp r8w,0001
+			code << "74 0E"; // je (14 steps forward)
+			code << process.JumpAbsolute(0x1030B4); // jump to shooting function end
 
-			process.InjectX64(0x129FDE, 9, code);
+			// Stolen code
+			code << "88 54 24 10"; // mov [rsp+10], dl
+			code << "55"; // push rbp
+			code << "56"; // push rsi
+
+			code << "41 54"; // push r12
+			code << "41 55"; // push r13
+			code << "41 57"; // push r15
+			code << "48 8D AC 24 E0 FC FF FF"; // lea rbp,[rsp-00000320]
+			
+			process.InjectX64(0x1028A0, 6, code);
 		}
 	}
 	catch (const CmdArgs::MissingArguments& e)
