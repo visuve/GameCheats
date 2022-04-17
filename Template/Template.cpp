964b656a6017c98e700d49fc7b1800650126411e
visuve <veli-matti.visuri@iki.fi>
2022-04-17 21:07:32 +0300

    Simplify CMake files & add template project
    - The template is also a useful testbed to just fool around

---
+++ Template/Template.cpp
@@ -0,0 +1,16 @@
+#include "../Mega.pch"
+
+int wmain(int argc, wchar_t** argv)
+{
+	try
+	{
+		// Use this as a template, or start hacking away!
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return -1;
+	}
+
+	return 0;
+}
