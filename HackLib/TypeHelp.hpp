56cd78824bffa4885e6c4a48765c5ff618f6d2a9
visuve <veli-matti.visuri@iki.fi>
2022-04-29 12:27:52 +0300

    Make TypeHelp.hpp more compact

--- HackLib/TypeHelp.hpp
+++ HackLib/TypeHelp.hpp
@@ -2,180 +2,71 @@
 
 inline size_t SizeOfBasicType(std::type_index type)
 {
-	if (type == typeid(Pointer))
-	{
-		return Pointer::Size;
-	}
-	else if (type == typeid(uint8_t))
-	{
-		return sizeof(uint8_t);
-	}
-	else if (type == typeid(uint16_t))
-	{
-		return sizeof(uint16_t);
-	}
-	else if (type == typeid(uint32_t))
-	{
-		return sizeof(uint32_t);
-	}
-	else if (type == typeid(uint64_t))
-	{
-		return sizeof(uint64_t);
-	}
-	else if (type == typeid(int8_t))
-	{
-		return sizeof(int8_t);
-	}
-	else if (type == typeid(int16_t))
-	{
-		return sizeof(int16_t);
-	}
-	else if (type == typeid(int32_t))
-	{
-		return sizeof(int32_t);
-	}
-	else if (type == typeid(int64_t))
-	{
-		return sizeof(int64_t);
-	}
-	else if (type == typeid(float))
-	{
-		return sizeof(float);
-	}
-	else if (type == typeid(double))
-	{
-		return sizeof(double);
-	}
-	else if (type == typeid(uint8_t*))
-	{
-		return sizeof(uint8_t*);
-	}
-	else if (type == typeid(uint16_t*))
-	{
-		return sizeof(uint16_t*);
-	}
-	else if (type == typeid(uint32_t*))
-	{
-		return sizeof(uint32_t*);
-	}
-	else if (type == typeid(uint64_t*))
-	{
-		return sizeof(uint64_t*);
-	}
-	else if (type == typeid(int8_t*))
-	{
-		return sizeof(int8_t*);
-	}
-	else if (type == typeid(int16_t*))
-	{
-		return sizeof(int16_t*);
-	}
-	else if (type == typeid(int32_t*))
-	{
-		return sizeof(int32_t*);
-	}
-	else if (type == typeid(int64_t*))
-	{
-		return sizeof(int64_t*);
-	}
-	else if (type == typeid(float*))
-	{
-		return sizeof(float*);
-	}
-	else if (type == typeid(double*))
-	{
-		return sizeof(double*);
-	}
+	if (type == typeid(Pointer)) return Pointer::Size;
+	if (type == typeid(uint8_t)) return sizeof(uint8_t);
+	if (type == typeid(uint16_t)) return sizeof(uint16_t);
+	if (type == typeid(uint32_t)) return sizeof(uint32_t);
+	if (type == typeid(uint64_t)) return sizeof(uint64_t);
+	if (type == typeid(int8_t)) return sizeof(int8_t);
+	if (type == typeid(int16_t)) return sizeof(int16_t);
+	if (type == typeid(int32_t)) return sizeof(int32_t);
+	if (type == typeid(int64_t)) return sizeof(int64_t);
+	if (type == typeid(float)) return sizeof(float);
+	if (type == typeid(double)) return sizeof(double);
+	if (type == typeid(uint8_t*)) return sizeof(uint8_t*);
+	if (type == typeid(uint16_t*)) return sizeof(uint16_t*);
+	if (type == typeid(uint32_t*)) return sizeof(uint32_t*);
+	if (type == typeid(uint64_t*)) return sizeof(uint64_t*);
+	if (type == typeid(int8_t*)) return sizeof(int8_t*);
+	if (type == typeid(int16_t*)) return sizeof(int16_t*);
+	if (type == typeid(int32_t*)) return sizeof(int32_t*);
+	if (type == typeid(int64_t*)) return sizeof(int64_t*);
+	if (type == typeid(float*)) return sizeof(float*);
+	if (type == typeid(double*)) return sizeof(double*);
 
 	return 0;
 }
 
 inline std::string_view BasicTypeToString(std::type_index type)
 {
-	if (type == typeid(Pointer))
-	{
-		return "\"Pointer\"";
-	}
-	else if (type == typeid(uint8_t))
-	{
-		return "uint8_t";
-	}
-	else if (type == typeid(uint16_t))
-	{
-		return "uint16_t";
-	}
-	else if (type == typeid(uint32_t))
-	{
-		return "uint32_t";
-	}
-	else if (type == typeid(uint64_t))
-	{
-		return "uint64_t";
-	}
-	else if (type == typeid(int8_t))
-	{
-		return "int8_t";
-	}
-	else if (type == typeid(int16_t))
-	{
-		return "int16_t";
-	}
-	else if (type == typeid(int32_t))
-	{
-		return "int32_t";
-	}
-	else if (type == typeid(int64_t))
-	{
-		return "int64_t";
-	}
-	else if (type == typeid(float))
-	{
-		return "float";
-	}
-	else if (type == typeid(double))
-	{
-		return "double";
-	}
-	else if (type == typeid(uint8_t*))
-	{
-		return "uint8_t*";
-	}
-	else if (type == typeid(uint16_t*))
-	{
-		return "uint16_t*";
-	}
-	else if (type == typeid(uint32_t*))
-	{
-		return "uint32_t*";
-	}
-	else if (type == typeid(uint64_t*))
-	{
-		return "uint64_t*";
-	}
-	else if (type == typeid(int8_t*))
-	{
-		return "int8_t*";
-	}
-	else if (type == typeid(int16_t*))
-	{
-		return "int16_t*";
-	}
-	else if (type == typeid(int32_t*))
-	{
-		return "int32_t*";
-	}
-	else if (type == typeid(int64_t*))
-	{
-		return "int64_t*";
-	}
-	else if (type == typeid(float*))
-	{
-		return "float*";
-	}
-	else if (type == typeid(double*))
-	{
-		return "double*";
-	}
-
+	if (type == typeid(Pointer)) return "\"Pointer\"";
+	if (type == typeid(uint8_t)) return "uint8_t";
+	if (type == typeid(uint16_t)) return "uint16_t";
+	if (type == typeid(uint32_t)) return "uint32_t";
+	if (type == typeid(uint64_t)) return "uint64_t";
+	if (type == typeid(int8_t)) return "int8_t";
+	if (type == typeid(int16_t)) return "int16_t";
+	if (type == typeid(int32_t)) return "int32_t";
+	if (type == typeid(int64_t)) return "int64_t";
+	if (type == typeid(float)) return "float";
+	if (type == typeid(double)) return "double";
+	if (type == typeid(uint8_t*)) return "uint8_t*";
+	if (type == typeid(uint16_t*)) return "uint16_t*";
+	if (type == typeid(uint32_t*)) return "uint32_t*";
+	if (type == typeid(uint64_t*)) return "uint64_t*";
+	if (type == typeid(int8_t*)) return "int8_t*";
+	if (type == typeid(int16_t*)) return "int16_t*";
+	if (type == typeid(int32_t*)) return "int32_t*";
+	if (type == typeid(int64_t*)) return "int64_t*";
+	if (type == typeid(float*)) return "float*";
+	if (type == typeid(double*)) return "double*";
+	
 	return "unknown type";
+}
+
+inline bool IsBasicPointer(std::type_index type)
+{
+	if (type == typeid(Pointer)) return true;
+	if (type == typeid(uint8_t*)) return true;
+	if (type == typeid(uint16_t*)) return true;
+	if (type == typeid(uint32_t*)) return true;
+	if (type == typeid(uint64_t*)) return true;
+	if (type == typeid(int8_t*)) return true;
+	if (type == typeid(int16_t*)) return true;
+	if (type == typeid(int32_t*)) return true;
+	if (type == typeid(int64_t*)) return true;
+	if (type == typeid(float*)) return true;
+	if (type == typeid(double*)) return true;
+
+	return false;
 }
