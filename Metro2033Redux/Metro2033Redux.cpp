22cead19d6fe0d22898d162160ee858f19c92484
visuve <veli-matti.visuri@iki.fi>
2022-11-27 18:21:25 +0200

    Improve Metro 2033 cheats

--- Metro2033Redux/Metro2033Redux.cpp
+++ Metro2033Redux/Metro2033Redux.cpp
@@ -23,14 +23,24 @@ int IWillNotUseHackLibForEvil(const std::vector<std::string>& givenArguments)
 
 	if (args.Contains("infammo"))
 	{
-		// Reload related
 		// Original: mov [rcx+000004F4],edx
 		process.Fill(0x2B2729, 0x2B272F, X86::Nop);
 
-		// Shooting itself
+		// NOTE: a NOP approach does not work when picking up new guns
 		// Original: mov [rsi+00000448], r14d
-		process.Fill(0x2C0B2F, 0x2C0B36, X86::Nop);
+		// process.Fill(0x2C0B2F, 0x2C0B36, X86::Nop);
 
+		// There is extra space, hence I do not need to allocate new memory
+		ByteStream hack;
+		hack << "41 BE FF 00 00 00"; // mov r14d,000000FF
+		hack << "44 89 B6 48 04 00 00"; // mov [rsi+00000448],r14d <- stolen
+		hack << "EB C2";
+		process.WriteBytes(0x2C0B60, hack);
+
+		ByteStream jump;
+		jump << "EB 2F 90 90 90 90 90";
+
+		process.WriteBytes(0x2C0B2F, jump);
 	}
 
 	return 0;
