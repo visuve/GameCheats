4293d6e8f872a38ff124d636c95978094a9dd240
visuve <veli-matti.visuri@iki.fi>
2023-05-01 15:01:50 +0300

    Add cheats for Doom Eternal
    - Health and armor can be set from command line

--- DoomEternal/DoomEternal.cpp
+++ DoomEternal/DoomEternal.cpp
@@ -15,6 +15,9 @@ int IWillNotUseHackLibForEvil(const std::vector<std::string>& givenArguments)
 		{ "infammo", typeid(std::nullopt), "Infinite ammo" },
 		{ "freeupgrades", typeid(std::nullopt), "Free weapon & gear upgrades" },
 		{ "coollauncher", typeid(std::nullopt), "No cooldown for equipment launcher (grenades & flame)" },
+		{ "coolguns", typeid(std::nullopt), "No cooldown for guns" },
+		{ "health", typeid(float), "Set current and max health. Default 100." },
+		{ "armor", typeid(float), "Set current and max armor. Default 50." },
 	});
 
 	DWORD pid = System::WaitForExe(L"DOOMEternalx64vk.exe");
@@ -40,6 +43,8 @@ int IWillNotUseHackLibForEvil(const std::vector<std::string>& givenArguments)
 		process.ChangeBytes(0x1D24A31,
 			ByteStream("89 7B 40"), // mov [rbx+40], edi
 			ByteStream("90 90 90")); // mov [rbx+40], edi
+
+		Log << "Infinite ammo applied";
 	}
 
 	if (args.Contains("freeupgrades"))
@@ -47,6 +52,8 @@ int IWillNotUseHackLibForEvil(const std::vector<std::string>& givenArguments)
 		process.ChangeBytes(0x1A4899F,
 			ByteStream("44 01 84 B1 84 CD 04 00"), // add [rcx+rsi*4+0004CD84],r8d
 			ByteStream("FF 84 B1 84 CD 04 00 90")); // inc [rcx+rsi*4+0004CD84]; nop
+
+		Log << "Free upgrades applied";
 	}
 
 	if (args.Contains("coollauncher"))
@@ -58,6 +65,81 @@ int IWillNotUseHackLibForEvil(const std::vector<std::string>& givenArguments)
 		process.ChangeBytes(0xA5F4654,
 			ByteStream("FF 8F E0 6F 00 00"), // dec [rdi+00006FE0]
 			ByteStream("FF 87 E0 6F 00 00")); // inc [rdi+00006FE0]
+
+		Log << "Cool launcher applied";
+	}
+
+	Pointer doomGuy;
+
+	if (args.Contains("health") || args.Contains("armor"))
+	{
+		// All of the above appear to work, but I just picked the nicest
+		// doomGuy = process.ResolvePointer(0x06BC2270, 0x38, 0x5DCu, 0x58u);
+		// doomGuy = process.ResolvePointer(0x06BC2270, 0x1F0, 0x52Cu, 0x58u);
+		doomGuy = process.ResolvePointer(0x06BC2270, 0x0u, 0x20u, 0x58u);
+		// doomGuy = process.ResolvePointer(0x06BC2270, 0x400, 0x5DCu, 0x58u);
+		// doomGuy = process.ResolvePointer(0x06BC2270, 0x1F0, 0x82Cu, 0x58u);
+		LogVariableHex(doomGuy.Value());
+	}
+
+	if (args.Contains("health"))
+	{
+		float wantedHealth = args.Value<float>("health", 100.0f);
+
+		Pointer maxHealthPtr = doomGuy + 0x38u;
+		Pointer healthPtr = doomGuy + 0x44u;
+
+		LogVariableHex(maxHealthPtr.Value());
+		LogVariableHex(healthPtr.Value());
+
+		float maxHealth = process.Read<float>(maxHealthPtr);
+		float health = process.Read<float>(healthPtr);
+
+		LogDebug << "Before:";
+		LogVariable(maxHealth);
+		LogVariable(health);
+
+		process.Write(maxHealthPtr, wantedHealth);
+		process.Write(healthPtr, wantedHealth);
+
+		maxHealth = process.Read<float>(maxHealthPtr);
+		health = process.Read<float>(healthPtr);
+
+		LogDebug << "After:";
+		LogVariable(maxHealth);
+		LogVariable(health);
+
+		Log << "Health updated";
+	}
+
+	if (args.Contains("armor"))
+	{
+		float wantedArmor = args.Value<float>("armor", 50.0f);
+
+		Pointer maxArmorPtr = doomGuy + 0xE8;
+		Pointer armorPtr = doomGuy + 0xF4;
+
+		LogVariableHex(maxArmorPtr.Value());
+		LogVariableHex(armorPtr.Value());
+
+		float maxArmor = process.Read<float>(maxArmorPtr);
+		float armor = process.Read<float>(armorPtr);
+
+		LogDebug << "Before:";
+		LogVariable(maxArmor);
+		LogVariable(armor);
+
+		process.Write(maxArmorPtr, wantedArmor);
+		process.Write(armorPtr, wantedArmor);
+
+		maxArmor = process.Read<float>(maxArmorPtr);
+		armor = process.Read<float>(armorPtr);
+
+		LogDebug << "After:";
+		LogVariable(maxArmor);
+		LogVariable(armor);
+
+		Log << "Armor updated";
 	}
 
 	return exitCode;
