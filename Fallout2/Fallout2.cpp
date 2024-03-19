ac1221786ae3441bd5bc6f4810dcda1cc3d7eb4f
visuve <veli-matti.visuri@iki.fi>
2024-03-19 19:21:01 +0200

    Add more cheats for Fallout 2

--- Fallout2/Fallout2.cpp
+++ Fallout2/Fallout2.cpp
@@ -5,7 +5,9 @@ int IWillNotUseHackLibForEvil(const std::vector<std::string>& givenArguments)
 	const CommandLine args(givenArguments,
 	{
 		{ "charpoints", typeid(int), "Set character points" },
-		{ "skillpoints", typeid(int), "Set skill points" }
+		{ "actionpoints", typeid(int), "Set action points" },
+		{ "skillpoints", typeid(int), "Set skill points" },
+		{ "money", typeid(int), "Set money" },
 	});
 
 	DWORD pid = System::WaitForExe(L"fallout2HR.exe");
@@ -28,11 +30,28 @@ int IWillNotUseHackLibForEvil(const std::vector<std::string>& givenArguments)
 		process.Write<int32_t>(0x118538, charpoints);
 	}
 
+	if (args.Contains("actionpoints"))
+	{
+		int actionpoints = args.Value<int>("actionpoints");
+		process.Write<int32_t>(0x11C3B4, actionpoints);
+	}
+
 	if (args.Contains("skillpoints"))
 	{
 		// The individual skills are at 0x11C4AC-0x11C4F0
 		int skillpoints = args.Value<int>("skillpoints");
 		process.Write<int32_t>(0x2681AC, skillpoints);
+	}
+
+	if (args.Contains("money"))
+	{
+		Pointer moneyPtr = process.ResolvePointer(0x119058, 0x34, 0x1C);
+
+		int before = process.Read<int32_t>(moneyPtr);
+		int after = args.Value<int>("money");
+		process.Write(moneyPtr, after);
+
+		LogInfo << "Money:" << before << "->" << after;
 	}
 
 	return 0;
